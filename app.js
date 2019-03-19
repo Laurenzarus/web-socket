52343743320609bf02475528cdaff7f0620b907e
Laurence Fong <laurencf@usc.edu>
2019-03-19 05:06:24 -0700

    edit

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ let wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 console.log("websocket code being run!");
 
+var content = "";
+
 wss.on('connection', (ws) => {
+
   ws.on('message', (message) => {
     console.log(`Received new content: ${message}`);
-
+    content = message;
     wss.clients.forEach((client) => {
-      client.send(message);
+      client.send(content);
     });
 
   });
