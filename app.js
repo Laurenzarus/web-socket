4c49344acfa819d99ef51c4b79e14da4c687415b
Laurence Fong <laurencf@usc.edu>
2019-03-19 04:59:22 -0700

    minor change

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let WebSocket = require('ws');
 
 let wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
+console.log("websocket code being run!");
+
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log(`Received new content: ${message}`);
